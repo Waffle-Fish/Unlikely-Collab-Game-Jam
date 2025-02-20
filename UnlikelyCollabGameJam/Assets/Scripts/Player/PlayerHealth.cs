ede2f0fac2d147d09db9ae0e18d5c823afc23bbe
Jhoshua Ampo <ampojhoshua@gmail.com>
2025-02-20 10:30:31 -0800

    implement health

--- UnlikelyCollabGameJam/Assets/Scripts/Player/PlayerHealth.cs
+++ UnlikelyCollabGameJam/Assets/Scripts/Player/PlayerHealth.cs
@@ -2,15 +2,48 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Health Settings")]
+    [SerializeField]
+    private float maxHealth = 100f;
+    public float CurrentHealth { get; private set; }
+
+    [Header("For Testing Purposes")]
+    public bool ClickToHeal = false;
+    public bool ClickToDamage = false;
+
+    private void Start()
     {
-        
+        CurrentHealth = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        // For testing purposes
+        if (ClickToHeal) {
+            ClickToHeal = false;
+            Heal(10);
+        }
+        if (ClickToDamage) {
+            ClickToDamage = false;
+            TakeDamage(10);
+        }
     }
+
+    public void TakeDamage(float damage)
+    {
+        CurrentHealth -= damage;
+        Debug.Log("Ouch! I only have " + CurrentHealth + " health left!");
+        if (CurrentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    public void Heal(float healAmount)
+    {
+        CurrentHealth = Mathf.Clamp(CurrentHealth + healAmount , 0, maxHealth);
+    }
+
+    private void Die()
+    {
+        gameObject.SetActive(false);
+    }   
 }
