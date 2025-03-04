e4603eaa3cae5b745fa940654f4bace10d068a67
Jhoshua Ampo <ampojhoshua@gmail.com>
2025-03-03 16:12:25 -0800

    implement pause menu

---
+++ UnlikelyCollabGameJam/Assets/Scripts/Utility/PauseGame.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public void Pause() {
+        Time.timeScale = 0;
+    }
+
+    public void Resume() {
+        Time.timeScale = 1;
+    }
+}
